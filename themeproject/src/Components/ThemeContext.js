268faafb54a5e6db1920fc7547561c7e1f441fad
Dearborn42 <amurf26@outlook.com>
2024-02-01 10:58:49 -0700

    Finished ThemeContext for updatable theme

--- themeproject/src/Components/ThemeContext.js
+++ themeproject/src/Components/ThemeContext.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import App from '../App';
+
+const ThemeContextWrapper = React.createContext();
 
 const ThemeContext = () => {
-  return (
-    <div>ThemeContext</div>
-  )
+    const [theme, setTheme] = useState("dark");
+    const darkMode = () => {
+        if(theme === "dark")
+            return setTheme("light");
+        return setTheme("dark");
+    }
+    return (
+        <ThemeContextWrapper.Provider value={{darkMode, theme}}>
+            <App />
+        </ThemeContextWrapper.Provider>
+    )
 }
 
 export default ThemeContext
